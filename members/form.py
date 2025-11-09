fb9248d15f4895c5ef7d365527c5395c2100ab57
jeremymordecai628 <125563448+jeremymordecai628@users.noreply.github.com>
2025-11-09 16:23:46 +0000

    Update form.py

--- members/form.py
+++ members/form.py
@@ -1,17 +1,3 @@
 from django import forms
 from .models import MeetingMinutes, OrganizationInvitation
 
-class MinutesUploadForm(forms.ModelForm):
-    """
-    Form for uploading or editing meeting minutes.
-    """
-    class Meta:
-        model = MeetingMinutes
-        fields = ['document', 'notes']
-
-
-class OrganizationInvitationForm(forms.ModelForm):
-    class Meta:
-        model = OrganizationInvitation
-        fields = ['name', 'email', 'contact_person', 'phone', 'message']
-
